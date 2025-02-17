989d14c433f1f86d078f4daea2adec97271d35a1
sufaid41578 <ss9325@srmist.edu.in>
2025-02-17 23:37:52 +0530

    Add files via upload

---
+++ rock-paper-scissors/rock-paper-scissors/script.js
@@ -0,0 +1,54 @@
+const gameContainer = document.querySelector(".container");
+const userResult = document.querySelector(".user_result img");
+const botResult = document.querySelector(".bot_result img");
+const result = document.querySelector(".result");
+const optionImages = document.querySelectorAll(".option_image");
+
+const botImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+const outcomes = {
+  RR: "Draw",
+  RP: "BOT",
+  RS: "YOU",
+  PP: "Draw",
+  PR: "YOU",
+  PS: "BOT",
+  SS: "Draw",
+  SR: "BOT",
+  SP: "YOU"
+};
+
+function handleOptionClick(event) {
+  const clickedImage = event.currentTarget;
+  const clickedIndex = Array.from(optionImages).indexOf(clickedImage);
+
+  userResult.src = botResult.src = "images/rock.png";
+  result.textContent = "Wait...";
+
+  optionImages.forEach((image, index) => {
+    image.classList.toggle("active", index === clickedIndex);
+  });
+
+  gameContainer.classList.add("start");
+
+  setTimeout(() => {
+    gameContainer.classList.remove("start");
+
+    const userImageSrc = clickedImage.querySelector("img").src;
+    userResult.src = userImageSrc;
+
+    const randomNumber = Math.floor(Math.random() * botImages.length);
+    const botImageSrc = botImages[randomNumber];
+    botResult.src = botImageSrc;
+
+    const userValue = ["R", "P", "S"][clickedIndex];
+    const botValue = ["R", "P", "S"][randomNumber];
+    const outcomeKey = userValue + botValue;
+    const outcome = outcomes[outcomeKey] || "Unknown";
+
+    result.textContent = userValue === botValue ? "Match Draw" : `${outcome} WON!`;
+  }, 2500);
+}
+
+optionImages.forEach(image => {
+  image.addEventListener("click", handleOptionClick);
+});
